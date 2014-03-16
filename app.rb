b7a32f4d7f95bee60cdcfc95e54dd98ff6563622
Noxmortis <iramortis@gmail.com>
2014-03-16 18:46:19 -0400

    format

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ end
 get '/css/:name.css' do
   content_type 'text/css', :charset => 'utf-8'
   filename = params[:name]
-  render :scss, filename.to_sym, :layout => false, :views =>'./public/css/sass'
+  render :scss, filename.to_sym, :layout => false, :views => './public/css/sass'
 end
 
 get '/' do
