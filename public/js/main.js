f60e07f85da9d0e86a1730aa45dbbeb150dcf24c
Noxmortis <iramortis@gmail.com>
2014-04-12 01:36:16 -0400

    make left menu mini

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,16 @@
 $(function(){
+  var nav = $('nav *');
+  nav.hide();
 
+  $('header h1').on('click', function(){
+    if(nav.is(':hidden')){
+      nav.slideDown('fast');
+    }else{
+      nav.slideUp('fast');
+    }
+  });
+
+  $('nav .close').on('click', function(){
+    nav.slideUp('fast');
+  });
 });
